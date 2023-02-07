800ffc74ddf38c0c10a8bfe149605a85336477d5
Harsh Mnothiya <114900961+Harsmnothiya-bit@users.noreply.github.com>
2023-02-07 12:42:14 +0530

    Create Certificate.jsx

---
+++ Certificate.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Certificates() {
+    return (
+        <div className='max-w-5xl mx-auto p-5'><h1 className='text-4xl font-semibold'>
+            Certificates
+        </h1>
+        <div>
+        <div className='p-5 border-2 m-3 max-w-lg'>
+            <div>
+                <h1 className='text-2xl'>
+                    DSA by geeksforgeeks
+                </h1>
+                <img src="https://media.licdn.com/dms/image/C4D22AQH9gNHKQkrR6g/feedshare-shrink_800/0/1668609059490?e=1678320000&v=beta&t=eHuDeID535hexwHrD2iULzw5Qwmi150zS8hwku5Fp6U" alt="" />
+            </div>
+        </div>
+        <div className='p-5 border-2 m-3 max-w-lg'>
+            <div>
+                <h1 className='text-2xl'>
+                    Data Analyist By linkedin learning
+                </h1>
+                <img src="https://media.licdn.com/dms/image/sync/C4D1FAQH-1j65boFTzA/feedshare-document-cover-images_800/0/1674965064328?e=1676314800&v=beta&t=qJko05L5yHeN7LYO7gLizNopdGXZvVUoFWvwXz1u7Cw" alt="" />
+            </div>
+        </div>
+        
+        </div>
+        </div>
+    )
+}
+
+export default Certificates
