09073bae137e84d5a4ede0ebd0739fafd0430e95
Harsh Mnothiya <114900961+Harsmnothiya-bit@users.noreply.github.com>
2023-02-07 12:42:59 +0530

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,21 @@
+function Header(){
+    return (
+        <section className="sticky top-0 z-10 bg-whitea">
+            <div className="shadow-lg border-b-black">
+                <div className="max-w-5xl mx-auto">
+                    <div className="p-5 flex flex-row justify-between">
+                        <div>
+                            <h1  
+                            className="italic font-semibold lg:text-2xl md:text-xl text-lg"
+                            >
+                                Harsh Mnothiya
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Header
