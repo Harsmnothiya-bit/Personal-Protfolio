7ff8d1d4ee341f0f8cf1eafbf1eed5858d9f3624
Harsh Mnothiya <114900961+Harsmnothiya-bit@users.noreply.github.com>
2023-02-07 12:44:50 +0530

    personal project

---
+++ Project.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div className='max-w-5xl mx-auto p-5'>
+        <h1 className='text-4xl font-semibold'>
+            Projects
+        </h1>
+        <div className='p-5 border-2 m-3 max-w-lg'>
+            <div>
+                <h1 className='text-2xl border-b-2 pb-3'>
+                    Rock Paper Scissors
+                </h1>
+                <p className='mt-4'>
+                    A simple rock paper scissors game made with java and concpets of Data structures and algorithms
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Project
